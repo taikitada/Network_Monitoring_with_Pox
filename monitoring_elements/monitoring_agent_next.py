4c232be5518e8192b66481a7607166cf74cd8f07
TaikiTada <mr.strangebird@gmail.com>
2013-01-11 15:53:44 +0900

    recommit, cause last commit is deleted

--- monitoring_elements/monitoring_agent_next.py
+++ monitoring_elements/monitoring_agent_next.py
@@ -13,20 +13,28 @@ from lib.ConfigParserExtend import *
 CP = ConfigParserExtend("server.conf")
 ConfigDict = CP.ReturnConfigDict()
 
+class Monitoring_Agent(rpyc.Service):
+	def on_connect(self):
+		print "made a connection with Monitoring_Server"
 
-class Monitoring_Agent:
-	def __init__(self):
-		self.sshctx = SshContext(ConfigDict['Monitoring_Server']['host'],
-							ConfigDict['Monitoring_Server']['user'],
-							port = None,
-							ConfigDict['Monitoring_Server']['keyfile'])
-		self.conn = rpyc.ssh_connect(self.sshctx, ConfigDict['Monitoring_Server']['port'])
-		(self.conn).root.register_agent()
-	def exposed_get_rtt():
-		get_rtt
-		pass
+	def on_disconnect(self):
+		print "A connection is dropped."
+
+	def exposed_get_rtt(self, dest_host):
+		return get_rtt(dest_host)
+
 
 if __name__ == '__main__':
-
-
+	switch_dpid = datapath_id_for_pox(ConfigDict['Switch']['switch'])
+	sshctx = SshContext(ConfigDict['Monitoring_Server']['host'],
+							ConfigDict['Monitoring_Server']['user'],
+							port = None,
+							keyfile = ConfigDict['Monitoring_Server']['keyfile'])
+	print self.sshctx
+	conn = rpyc.ssh_connect(sshctx, int(ConfigDict['Monitoring_Server']['port']))
+	print (conn).root.register_agent(ConfigDict['Monitoring_Agent']['host'],
+										ConfigDict['Monitoring_Agent']['port'], switch_dpid)
+	from rpyc.utils.server import ThreadedServer
+	s = ThreadedServer(Monitoring_Agent, port = int(ConfigDict['Monitoring_Agent']['port']))
+	s.start()
 
--- monitoring_elements/monitoring_server_next.py
+++ monitoring_elements/monitoring_server_next.py
@@ -3,8 +3,13 @@ monitoring_server_next
 
 '''
 import rpyc
-import sys
 from collections import defaultdict
+import ConfigParser
+import sys,os
+from os import path
+
+CURRENT_DIR=path.dirname(os.path.abspath(__file__))
+sys.path.append(CURRENT_DIR + '/' + "../")
 
 # [k.dpid][j.dpid]->distance
 # overlapping is nothing
@@ -13,6 +18,10 @@ link_distance = defaultdict(lambda:defaultdict(lambda:None))
 
 # switch_dpid -> (agent_ip, agent_port)
 agent_switch_map = {}
+
+from lib.ConfigParserExtend import *
+CP = ConfigParserExtend("server.conf")
+ConfigDict = CP.ReturnConfigDict()
 
 class Monitoring_Server(rpyc.Service):
 	def exposed_register_agent(self, server_ip, server_port, switch_dpid):
@@ -41,8 +50,15 @@ class Monitoring_Server(rpyc.Service):
 				(changed_to_dict[a]).update({b:c})
 		return changed_to_dict
 
+	def on_connect(self):
+		print "made a connection with Monitoring_Server"
+
+	def on_disconnect(self):
+		print "A connection is dropped."
+
 
 if __name__ == '__main__':
 	from rpyc.utils.server import ThreadedServer
-	s = ThreadedServer(DoStuffService, port = 15555)
+	print int(ConfigDict['Monitoring_Server']['port'])
+	s = ThreadedServer(Monitoring_Server, port = int(ConfigDict['Monitoring_Server']['port']))
 	s.start()
